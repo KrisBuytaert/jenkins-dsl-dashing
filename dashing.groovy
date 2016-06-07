dfbc7ea68c88a141d6c372b9e764598bf6d11d97
Kris Buytaert <sdog@inuits.eu>
2016-06-07 10:51:25 +0200

    Example 1

--- dashing.groovy
+++ dashing.groovy
@@ -1,20 +1,75 @@
+folder('dashing')
 
 job('dashing/dashing-dashboard-build') {
-    /** require tag docker */
     scm {
 
-        git{ remote { 
+        git{ remote {
             url('https://github.com/KrisBuytaert/build-dashing')
         }
+        /** require tag docker */
+
         }
     }
     triggers {
         scm('*/15 * * * *')
     }
     steps {
-        shell(readFileFromWorkspace('build-dashboard.sh'))
+        shell(readFileFromWorkspace('dashing/build-dashboard.sh'))
 
+    }
+    publishers {
+        downstreamParameterized {
+            trigger("dashing/dashing-dashboard-test") {
+                condition('SUCCESS')
+                    parameters {
+                        predefinedProp('ITERATION','${BUILD_NUMBER}')
+                            predefinedProp('GIT_COMMIT','${GIT_COMMIT}')
+                    }
+
+
+            }
+        }
+    }
+
+}
+
+
+
+job('dashing/dashing-dashboard-test') {
+    steps {
+        shell(readFileFromWorkspace('dashing/test-dashboard.sh'))
+    }
+    publishers {
+        downstreamParameterized {
+          trigger("dashing/dashing-dashboard-deploy") {
+                condition('SUCCESS')
+                    parameters {
+                        predefinedProp('ITERATION','${BUILD_NUMBER}')
+                        predefinedProp('GIT_COMMIT','${GIT_COMMIT}')
+                    }
+
+
+            }
+        }
+    }
+}
+job('dashing/dashing-dashboard-deploy') {
+    steps {
+        shell(readFileFromWorkspace('dashing/deploy-dashboard.sh'))
+            shell(readFileFromWorkspace('dashing/restart-dashboard.sh'))
     }
 }
 
-folder('dashing')
+
+
+job('dashing/dashing-build') {
+
+}
+
+
+buildPipelineView('dashing/dashboard-pipeline') {
+    title('Dashing Dashboard Pipeline')
+        displayedBuilds(5)
+        selectedJob('dashing/dashing-dashboard-build')
+        alwaysAllowManualTrigger()
+}
---
+++ dashing/deploy-dashboard.sh
@@ -0,0 +1,4 @@
+
+echo "Deploy Dashboard"
+
+
